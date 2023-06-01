fdf2ab21e5b86ddbbd57a805fe68c3cb6c39f3e7
28-png <murphy.d.matthew28@gmail.com>
2023-06-01 13:05:57 -0500

    updated

--- src/App.js
+++ src/App.js
@@ -34,19 +34,17 @@ function App() {
     },
   };
   
-
   return (
     <BrowserRouter>
-      <div className="App">
-      <div className="absolute inset-0 flex items-center justify-center">
+     <div className="absolute inset-0 flex items-center justify-center">
       <motion.div
         className="relative z-50 w-full bg-black"
         initial="initial"
         animate="animate"
           variants={blackBox}
-      />      
-   
-        <Navbar />
+      />
+      <div className="App">
+       <Navbar />
         <Routes>
           <Route
             path="/contact"
