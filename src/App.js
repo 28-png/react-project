433decffa4ed9cae8530630cd40f54837bcd75cd
28-png <murphy.d.matthew28@gmail.com>
2023-06-01 12:50:47 -0500

    updated animations

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Services from './components/Home/Services';
 import Contact from './components/ContactAndServer/Contact';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import PopupBox from './components/ContactAndServer/PopupBox';
+import { motion } from "framer-motion"
 
 
 function App() {
@@ -19,9 +20,32 @@ function App() {
     setIsPopupVisible(true);
   };
 
+  const blackBox = {
+    initial: {
+      height: "100vh",
+      bottom: 0,
+    },
+    animate: {
+      height: 0,
+      transition: {
+        duration: 1.5,
+        ease: [0.87, 0, 0.13, 1],
+      },
+    },
+  };
+  
+
   return (
     <BrowserRouter>
       <div className="App">
+      <div className="absolute inset-0 flex items-center justify-center">
+      <motion.div
+        className="relative z-50 w-full bg-black"
+        initial="initial"
+        animate="animate"
+          variants={blackBox}
+      />      
+   
         <Navbar />
         <Routes>
           <Route
@@ -33,6 +57,7 @@ function App() {
           <PopupBox message={popupMessage} onClose={handlePopupClose} />
         )}
         <Services />
+        </div>
       </div>
     </BrowserRouter>
   );
