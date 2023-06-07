79434aa898ab8da5cb15b7158eefaf7c3335832e
28-png <murphy.d.matthew28@gmail.com>
2023-06-07 12:28:41 -0500

    updated animation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Contact from './components/ContactAndServer/Contact';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import PopupBox from './components/ContactAndServer/PopupBox';
 import InitialTransition from './InitialTransition';
+import { motion } from "framer-motion";
 
 
 
@@ -21,12 +22,51 @@ function App() {
     setIsPopupVisible(true);
   };
   
+  const content = {
+    animate: {
+      transition: { staggerChildren: 0.1, delayChildren: 2.8 },
+    },
+  };
+  
+  const nav = {
+    initial: { y: -20, opacity: 0 },
+    animate: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.7,
+        ease: [0.6, -0.05, 0.01, 0.99],
+      },
+    },
+  };
+  
+  const services = {
+    initial: { y: -20, opacity: 0 },
+    animate: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.7,
+        ease: [0.6, -0.05, 0.01, 0.99],
+      },
+    },
+  };
+
   return (
     <div>
+      <motion.section exit={{ opacity: 0 }}>
       <InitialTransition />
+      <motion.div
+        initial="initial"
+        animate="animate"
+        variants={content}
+        className="space-y-12"
+      >
      <BrowserRouter>
      <div className="App">
+      <motion.section variants={nav}>
        <Navbar />
+       </motion.section>
         <Routes>
           <Route 
              path="/contact"
@@ -36,9 +76,13 @@ function App() {
          {isPopupVisible && (
            <PopupBox message={popupMessage} onClose={handlePopupClose} />
          )}
+         <motion.section variants={services}>
          <Services />
+         </motion.section>
          </div>
    </BrowserRouter>
+   </motion.div>
+   </motion.section>
    </div>
    
   );
